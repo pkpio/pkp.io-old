39be52b36149a4c8116dd3f30a6b7a0d15df83cf
Praveen Kumar Pendyala <praveendath92@gmail.com>
2015-09-14 21:17:45 +0200

    More projects to list

--- js/project.js
+++ js/project.js
@@ -4,7 +4,10 @@
 
 angular.module('PraveenApp').controller('ProjectCtrl', function($scope) {
     $scope.projects = [
+
+        // Coursestats
         {
+            showLevel: 5,
             badgeClass: 'info',
             year: "Now",
             title: 'Coursestats',
@@ -15,7 +18,7 @@ angular.module('PraveenApp').controller('ProjectCtrl', function($scope) {
             timeframe: "August 2015 - now",
             details: 'Grading statistics of courses at TU Darmstadt',
             skills: [
-                "AngularJS", "PHP5", "MySql", "Python"
+                "AngularJS", "PHP5", "MySql", "Python", "Google App Engine", "Material design"
             ],
             links: [
                 {
@@ -32,7 +35,10 @@ angular.module('PraveenApp').controller('ProjectCtrl', function($scope) {
                 }
             ]
         },
+
+        // MDroid
         {
+            showLevel: 5,
             badgeClass: 'info',
             year: "Now",
             title: 'MDroid',
@@ -42,7 +48,7 @@ angular.module('PraveenApp').controller('ProjectCtrl', function($scope) {
             },
             details: 'Moodle for Android',
             skills: [
-                "Android", "Markdown", "REST"
+                "Android", "Githooks", "Markdown", "REST", "Moodle Webservices"
             ],
             links: [
                 {
@@ -59,7 +65,14 @@ angular.module('PraveenApp').controller('ProjectCtrl', function($scope) {
                 }
             ]
         },
+
+    /**
+     *  **************  2015  ********************
+     */
+
+        // iRing
         {
+            showLevel: 3,
             badgeClass: 'default',
             year: "2015",
             title: 'iRing',
@@ -81,7 +94,226 @@ angular.module('PraveenApp').controller('ProjectCtrl', function($scope) {
                     url: "https://www.youtube.com/watch?v=88tF4xQ17BY&feature=youtu.be&start=22&end=60"
                 }
             ]
+        },
+
+        // Token bucket
+        {
+            showLevel: 2,
+            badgeClass: 'default',
+            year: "2015",
+            title: 'Token bucket',
+            icon:{
+                url: "img/projects/tokenbucket.png",
+                useUrl: true
+            },
+            details: 'A visualization of the Token Bucket',
+            skills: [
+                "JavaScript", "Bootstrap"
+            ],
+            links: [
+                {
+                    iconClass: "fa-github",
+                    url: "https://github.com/praveendath92/token-bucket"
+                },
+                {
+                    iconClass: "fa-globe",
+                    url: "http://praveen.xyz/demos/token-bucket/"
+                }
+            ]
+        },
+
+        // CollabTodo
+        {
+            showLevel: 2,
+            badgeClass: 'default',
+            year: "2015",
+            title: 'CollabTodo',
+            icon:{
+                url: "img/projects/tokenbucket.png",
+                useUrl: false
+            },
+            details: 'A fully decentralized, background syncing, collaborative Todo application',
+            skills: [
+                "Android", "PubSub"
+            ],
+            links: [
+                {
+                    iconClass: "fa-github",
+                    url: "https://github.com/praveendath92/CollabTodo"
+                }
+            ]
+        },
+
+        // ADK-Driver
+        {
+            showLevel: 4,
+            badgeClass: 'default',
+            year: "2015",
+            title: 'Android accessory driver',
+            icon:{
+                url: "img/projects/linux.png",
+                useUrl: true
+            },
+            details: 'Kernel driver to set any Android device into ADK (Accessory Development Kit) mode',
+            skills: [
+                "Linux", "Kernel driver", "Android accessory"
+            ],
+            links: [
+                {
+                    iconClass: "fa-github",
+                    url: "https://github.com/praveendath92/adk-driver"
+                }
+            ]
+        },
+
+
+    /**
+     *  **************  2014  ********************
+     */
+
+        // BARD
+        {
+            showLevel: 5,
+            badgeClass: 'warning',
+            year: "2014",
+            title: 'Google Summer of Code',
+            icon:{
+                url: "img/projects/gsoc.png",
+                useUrl: true
+            },
+            details: 'Android remote display for BeagleBoard and other Linux devices',
+            skills: [
+                "Linux", "Kernel driver", "Android", "Android accessory"
+            ],
+            links: [
+                {
+                    iconClass: "fa-linux",
+                    url: "https://github.com/praveendath92/bard-linux"
+                },
+                {
+                    iconClass: "fa-android",
+                    url: "https://github.com/praveendath92/bard-droid"
+                },
+                {
+                    iconClass: "fa-book",
+                    url: "https://github.com/praveendath92/bard-linux/blob/master/documentation/dev.md"
+                },
+                {
+                    iconClass: "fa-youtube",
+                    url: "https://www.youtube.com/watch?v=QymwVr9gY-Y"
+                },
+                {
+                    iconClass: "fa-wordpress",
+                    url: "http://praveenkumar.co.in/blog/?p=5"
+                }
+            ]
+        },
+
+        // PopTilesPlus
+        {
+            showLevel: 4,
+            badgeClass: 'warning',
+            year: "2014",
+            title: 'Pop Tiles Plus',
+            icon:{
+                url: "img/projects/poptilesplus.png",
+                useUrl: true
+            },
+            details: 'An extended version of the game PopTiles',
+            skills: [
+                "Android", "Playservices"
+            ],
+            links: [
+                {
+                    iconClass: "fa-github",
+                    url: "https://github.com/praveendath92/PopTilesPlus"
+                },
+                {
+                    iconClass: "fa-download",
+                    url: "https://play.google.com/store/apps/details?id=in.co.praveenkumar.poptilesplus"
+                }
+            ]
+        },
+
+        // TumTumTracker
+        {
+            showLevel: 5,
+            badgeClass: 'warning',
+            year: "2014",
+            title: 'Tum Tum Tracker',
+            icon:{
+                url: "img/projects/ttt.png",
+                useUrl: true
+            },
+            details: 'Android application to track the TumTums - internal transportation vehicles at IIT Bombay',
+            skills: [
+                "Android", "Google Maps"
+            ],
+            links: [
+                {
+                    iconClass: "fa-github",
+                    url: "https://github.com/praveendath92/TumTumTracker"
+                },
+                {
+                    iconClass: "fa-download",
+                    url: "https://play.google.com/store/apps/details?id=in.co.praveenkumar.tumtumtracker"
+                }
+            ]
+        },
+
+        // Cap.
+        {
+            showLevel: 3,
+            badgeClass: 'warning',
+            year: "2014",
+            title: 'Cap.',
+            icon:{
+                url: "img/projects/cap.png",
+                useUrl: true
+            },
+            details: 'A screen recorder for Android 4.4+ without root',
+            skills: [
+                "Android"
+            ],
+            links: [
+                {
+                    iconClass: "fa-github",
+                    url: "https://github.com/praveendath92/Cap."
+                }
+            ]
+        },
+
+
+    /**
+     *  **************  2013  ********************
+     */
+
+        // GroupChat.
+        {
+            showLevel: 4,
+            badgeClass: 'default',
+            year: "2013",
+            title: 'GroupChat',
+            icon:{
+                url: "img/projects/groupchat.png",
+                useUrl: true
+            },
+            details: 'A very basic group chat application for Android beginners',
+            skills: [
+                "Android"
+            ],
+            links: [
+                {
+                    iconClass: "fa-github",
+                    url: "https://github.com/praveendath92/GroupChat"
+                },
+                {
+                    iconClass: "fa-download",
+                    url: "https://play.google.com/store/apps/details?id=in.co.praveenkumar.groupchat"
+                }
+            ]
         }
+
 
     ];
 });
