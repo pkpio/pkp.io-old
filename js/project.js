ad9ff783c3cc05cc98b4788245f8d28b3321ca16
Praveen Kumar Pendyala <praveendath92@gmail.com>
2015-09-26 19:30:16 +0200

    Delayed call to data load in projects

--- js/project.js
+++ js/project.js
@@ -1,23 +1,28 @@
 /**
  * Created by praveen on 12.09.15.
  */
-angular.module('PraveenApp').controller('ProjectCtrl', function($scope, config, $http) {
+angular.module('PraveenApp').controller('ProjectCtrl', function($scope, $timeout, config, $http) {
     $scope.projects = null;
     $scope.ready = 0;
 
+    // Delayed call to avoid navbar freeze on close
+    $timeout(loadData, 200);
+
     // Get projects info
-    var req = {
-        method: 'GET',
-        url: config.baseUrl + '/data/project.json'
-    };
-    $http(req)
-        .then(
-        function (response) { // Success callback
-            $scope.projects = response.data;
-            $scope.ready = 1;
-        },
-        function (response) { //Error callback
-            console.log(response.toString());
-        }
-    );
+   $scope.loadData = function(){
+       var req = {
+           method: 'GET',
+           url: config.baseUrl + '/data/project.json'
+       };
+       $http(req)
+           .then(
+           function (response) { // Success callback
+               $scope.projects = response.data;
+               $scope.ready = 1;
+           },
+           function (response) { //Error callback
+               console.log(response.toString());
+           }
+       );
+   };
 });
