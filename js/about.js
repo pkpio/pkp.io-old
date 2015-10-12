d3c35eb16c31ec5d111b9eb06828a137bf132616
Praveen Kumar Pendyala <praveendath92@gmail.com>
2015-10-12 02:10:28 +0200

    About page content updated from json data

--- js/about.js
+++ js/about.js
@@ -2,6 +2,32 @@
  * Created by praveen on 12.09.15.
  */
 
-angular.module('PraveenApp').controller('AboutCtrl', function($scope, $timeout, config, $http) {
+angular.module('PraveenApp').controller('AboutCtrl', function($scope, $timeout, config, $http, $sce) {
+    $scope.profile = null;
+    $scope.ready = 0;
 
+    // Get profile
+    $scope.loadData = function(){
+        var req = {
+            method: 'GET',
+            url: 'data/about.json'
+        };
+        $http(req)
+            .then(
+            function (response) { // Success callback
+                $scope.profile = response.data;
+                $scope.ready = 1;
+            },
+            function (response) { //Error callback
+                console.log(response.toString());
+            }
+        );
+    };
+
+    $scope.renderHtml = function (htmlCode) {
+        return $sce.trustAsHtml(htmlCode);
+    };
+
+    // Delayed call to avoid navbar freeze on close
+    $timeout($scope.loadData, config.loadDelay);
 });
