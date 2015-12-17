cbb68e731555c834ca20052a162f06af0bb5defc
Praveen Kumar Pendyala <praveendath92@gmail.com>
2015-12-18 00:34:21 +0100

    Cleanup angular markdown dependency. We get html from jekyll outputs.

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@
  * Created by praveen on 12.09.15.
  */
 
-angular.module('PraveenApp', ['ngMaterial', 'ngRoute', 'angular-timeline', 'angularTypewrite', 'btford.markdown']);
+angular.module('PraveenApp', ['ngMaterial', 'ngRoute', 'angular-timeline', 'angularTypewrite']);
 
 // Theme setup
 angular.module('PraveenApp').config(function($mdThemingProvider) {
