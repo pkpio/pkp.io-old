aa932886f9d6c428a866c1e6a8d5eda7251b0ad2
Praveen Kumar Pendyala <praveendath92@gmail.com>
2015-12-23 01:11:04 +0100

    Project filters implemented

--- js/project.js
+++ js/project.js
@@ -25,5 +25,29 @@ angular.module('PraveenApp').controller('ProjectCtrl', function($scope, $timeout
 
     // Delayed call to avoid navbar freeze on close
     $timeout($scope.loadData, config.loadDelay);
-    
+
+    $scope.test = 0;
+
+    // Project filters
+    $scope.filters = {
+        year : {
+            options : ["2012", "2013", "2014", "2015", "Now", "All"],
+            value : "All"
+        },
+        showlevel : {
+            options : [1, 2, 3, 4, 5, "All"],
+            value : "All"
+        },
+        filter : function(project){
+            var yearMatch = false;
+            var levelMatch = false;
+
+            if($scope.filters.year.value == "All" || project.year == $scope.filters.year.value)
+                yearMatch = true;
+            if($scope.filters.showlevel.value == "All" || project.showLevel == $scope.filters.showlevel.value)
+                levelMatch = true;
+
+            return yearMatch && levelMatch;
+        }
+    }
 });
