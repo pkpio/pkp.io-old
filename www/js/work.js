d64c39802ba3503eadf93b33a271b2d054d26241
Praveen Kumar Pendyala <praveendath92@gmail.com>
2017-04-09 01:45:03 +0200

    remote service for work

--- www/js/work.js
+++ www/js/work.js
@@ -2,28 +2,11 @@
  * Created by praveen on 12.09.15.
  */
 
-angular.module('PraveenApp').controller('WorkCtrl', function($scope, $timeout, config, $http) {
-    $scope.works = null;
-    $scope.ready = 0;
-
+angular.module('PraveenApp').controller('WorkCtrl', function($scope, remote) {
     // Get works info
-    $scope.loadData = function(){
-        var req = {
-            method: 'GET',
-            url: config.baseUrl + '/data/work.json'
-        };
-        $http(req)
-            .then(
-            function (response) { // Success callback
-                $scope.works = response.data;
-                $scope.ready = 1;
-            },
-            function (response) { //Error callback
-                console.log(response.toString());
-            }
-        );
-    };
-
-    // Delayed call to avoid navbar freeze on close
-    $timeout($scope.loadData, config.loadDelay);
+    remote.fetchSiteData('/data/work.json').then(
+        function (data) {
+            $scope.works = data;
+        }
+    );
 });
