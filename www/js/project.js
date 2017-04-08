bc1c5e1086f518621d72bc1a83a47436f5b571e1
Praveen Kumar Pendyala <praveendath92@gmail.com>
2017-04-09 01:50:26 +0200

    projects updated with remote service

--- www/js/project.js
+++ www/js/project.js
@@ -1,32 +1,13 @@
 /**
  * Created by praveen on 12.09.15.
  */
-angular.module('PraveenApp').controller('ProjectCtrl', function($scope, $timeout, config, $http) {
-    $scope.projects = null;
-    $scope.ready = 0;
-
-    // Get projects info
-   $scope.loadData = function(){
-       var req = {
-           method: 'GET',
-           url: config.baseUrl + '/data/project.json'
-       };
-       $http(req)
-           .then(
-           function (response) { // Success callback
-               $scope.projects = response.data;
-               $scope.ready = 1;
-           },
-           function (response) { //Error callback
-               console.log(response.toString());
-           }
-       );
-   };
-
-    // Delayed call to avoid navbar freeze on close
-    $timeout($scope.loadData, config.loadDelay);
-
-    $scope.test = 0;
+angular.module('PraveenApp').controller('ProjectCtrl', function($scope, remote) {
+    // Get projects data
+    remote.fetchSiteData('/data/project.json').then(
+        function (data) {
+            $scope.projects = data;
+        }
+    );
 
     // Project filters
     $scope.filters = {
