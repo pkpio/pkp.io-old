644b6a71524c73327bdd61dbf75d627af1c3bd92
Praveen Kumar Pendyala <praveendath92@gmail.com>
2017-04-09 01:40:48 +0200

    academics page uses remote service

--- www/js/academics.js
+++ www/js/academics.js
@@ -2,31 +2,12 @@
  * Created by praveen on 14.09.15.
  */
 
-angular.module('PraveenApp').controller('AcadCtrl', function($scope, $timeout, config, $http) {
-    $scope.papers = null;
-    $scope.studies = null;
-    $scope.ready = 0;
-
-    // Get works info
-    $scope.loadData = function(){
-        var req = {
-            method: 'GET',
-            url: config.baseUrl + '/data/academics.json'
-        };
-        $http(req)
-            .then(
-            function (response) { // Success callback
-                $scope.papers = response.data.papers;
-                $scope.studies = response.data.studies;
-                $scope.ready = 1;
-            },
-            function (response) { //Error callback
-                console.log(response.toString());
-            }
-        );
-    };
-
-    // Delayed call to avoid navbar freeze on close
-    $timeout($scope.loadData, config.loadDelay);
-
+angular.module('PraveenApp').controller('AcadCtrl', function($scope, remote) {
+    // Get academic data
+    remote.fetchSiteData('/data/academics.json').then(
+        function (data) {
+            $scope.papers = data.papers;
+            $scope.studies = data.studies;
+        }
+    );
 });
